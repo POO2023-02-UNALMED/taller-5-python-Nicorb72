815a1c985a7c00618bdc6e4daba3ad77bda18706
Nicorb72 <141965721+Nicorb72@users.noreply.github.com>
2023-09-16 18:57:34 -0500

    :

--- zooAnimales/animal.py
+++ zooAnimales/animal.py
@@ -19,8 +19,8 @@ class Animal:
     @staticmethod
 
     def totalPorTipo():
-        return "Mamiferos: " + str(zooAnimales.mamifero.Mamifero.cantidadMamiferos()) + "\nAves: " + str(zooAnimales.ave.Ave.cantidadAves()) + "\nReptiles: " + str(zooAnimales.reptil.Reptil.cantidadReptiles()) + "\nPeces: "+ str(zooAnimales.pez.Pez.cantidadPeces()) + "\nAnfibios: "+ str(zooAnimales.anfibio.Anfibio.cantidadAnfibios())
-    
+        return "Mamiferos : " + str(zooAnimales.mamifero.Mamifero.cantidadMamiferos()) + "\nAves : " + str(zooAnimales.ave.Ave.cantidadAves()) + "\nReptiles : " + str(zooAnimales.reptil.Reptil.cantidadReptiles()) + "\nPeces : " + str(zooAnimales.pez.Pez.cantidadPeces()) + "\nAnfibios : " + str(zooAnimales.anfibio.Anfibio.cantidadAnfibios())
+               
     def toString(self):
         if self._zona != None:
             return "Mi nombre es " + self._nombre + ", tengo una edad de " + str(self._edad) + ", habito en " + self._habitat + " y mi genero es " + self._genero + ", la zona en la que me ubico es " + self._zona.getNombre() + ", en el zoo " + self._zona.getZoo().getNombre()
